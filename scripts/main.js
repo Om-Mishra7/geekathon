7d694ac4d4e7b09f00e8274ee4e39bfbc1ef7344
Om Mishra <contact@om-mishra.com>
2024-09-14 13:07:11 +0530

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {
+    const navbar = document.querySelector('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('navbar-scrolled');
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+    }
+}
+
